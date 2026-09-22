c6bdb261678fede24194f6feb17481cc0787effe
Thiago Alexandre <thiagokilu34@gmail.com>
2026-09-22 14:53:00 -0300

    fix: add schema params to route and fix controller typing

--- src/infra/http/controlers/search-users-controler.ts
+++ src/infra/http/controlers/search-users-controler.ts
@@ -1,17 +1,13 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import { makeSearchUsersUseCase } from "../../../app/use-cases/factories/make-search-users-usecase";
 
-interface SearchUsersParams {
-  username: string;
-}
-
 export async function searchUsersController(
-  request: FastifyRequest<{ Params: SearchUsersParams }>,
+  request: FastifyRequest,
   reply: FastifyReply,
 ) {
   try {
     const searchUsers = makeSearchUsersUseCase();
-    const { users } = await searchUsers({ username: request.params.username });
+    const { users } = await searchUsers({ username: (request.params as { username: string }).username });
 
     return reply.status(200).send({ message: "Users searched", users });
   } catch (error) {
--- src/infra/http/routes/search-users-route.ts
+++ src/infra/http/routes/search-users-route.ts
@@ -4,6 +4,7 @@ import { searchUsersController } from "../controlers/search-users-controler";
 import {
   searchUsersErrorResponseSchema,
   searchUsersSucessResponseSchema,
+  searchUsersSchema,
 } from "../schemas/search-users-schema";
 import { isAuth } from "../middlewares/isAuth";
 export function searchUsersRoute(app: FastifyInstance) {
@@ -20,13 +21,14 @@ export function searchUsersRoute(app: FastifyInstance) {
       schema: {
         summary: "Search users",
         tags: ["Users"],
+        params: searchUsersSchema,
         response: {
           200: searchUsersSucessResponseSchema,
           400: searchUsersErrorResponseSchema,
           500: searchUsersErrorResponseSchema,
         },
       },
-    }, 
+    },
     searchUsersController,
   );
 }
