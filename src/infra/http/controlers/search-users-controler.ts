ef133dabe29d5a4309c92b78c5ff1f40003a43c4
Thiago Alexandre <thiagokilu34@gmail.com>
2026-09-22 14:50:15 -0300

    fix: remove any type in search-users controller

--- src/infra/http/controlers/search-users-controler.ts
+++ src/infra/http/controlers/search-users-controler.ts
@@ -1,13 +1,17 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import { makeSearchUsersUseCase } from "../../../app/use-cases/factories/make-search-users-usecase";
 
+interface SearchUsersParams {
+  username: string;
+}
+
 export async function searchUsersController(
-  request: FastifyRequest,
+  request: FastifyRequest<{ Params: SearchUsersParams }>,
   reply: FastifyReply,
 ) {
   try {
     const searchUsers = makeSearchUsersUseCase();
-    const { users } = await searchUsers({ username: (request.params as any).username });
+    const { users } = await searchUsers({ username: request.params.username });
 
     return reply.status(200).send({ message: "Users searched", users });
   } catch (error) {
